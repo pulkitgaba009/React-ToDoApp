3335d87e4a18cb20094a456e2c4b95fc1a47381e
pulkitgaba009 <pulkitgaba009@gmail.com>
2025-06-19 17:42:39 +0530

    rewrote code

--- src/ToDoComponent.jsx
+++ src/ToDoComponent.jsx
@@ -1,69 +1,51 @@
-import { useState } from "react"
-import {v4 as uuidv4} from "uuid"
-
-let inputBtnStyle = {
-    height:"2.25rem",
-    borderRadius:"10px",
-    width:"15rem",
-}
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from "react";
+import Button from '@mui/material/Button'
 
 function ToDoComponent(){
+  let [todos,setTodo] = useState([{task:"Sample Task",taskState:true,id:uuidv4()}])
 
-    let [toDo,setToDo] = useState([{task:"Sample task",id:uuidv4()}])
-    let [newToDo,setNewToDo] = useState("")
+  function addTask(formData){
+    let newTask = formData.get("task")
 
-    let addNewTask = ()=>{
-    setToDo((prevTodo)=>{
-        return [...prevTodo,{task:newToDo,id:uuidv4()}]
+    setTodo((prevTodo)=>{
+      return [...prevTodo,{task:newTask,taskState:false,id:uuidv4()}]
     })
-    setNewToDo("")
-    }
+  }
 
+  function deleteTask(idToDel){
+    let deleteTask = todos.filter((t)=>t.id != idToDel)
+    setTodo(deleteTask);
+  }
 
-   let upperCaseAll = () => {
-    setToDo(
-        toDo.map((todo) => {
-            return {
-                ...todo,
-                task: todo.task.toUpperCase()
-            };
-        })
-    );
-};
-
-
-    let updateToDo =(event)=>{
-    setNewToDo(event.target.value)
-    }
-
-    let deleteTodo = (id) =>{
-        let copy = (toDo.filter((toDo)=> toDo.id != id));
-        setToDo(copy)
-    }
-
-    return(
-        <div>
-            <input style={inputBtnStyle} type="text" placeholder="Enter the task " value={newToDo} onChange={updateToDo}/>
-            &nbsp; &nbsp;
-            <button onClick={addNewTask}>Add Task</button>
-            <br/> 
-            <br />
-            <hr /> 
-    
-
-            <h4>To Do list:</h4>
-            <ul>{
-                toDo.map((toDo)=>{
-                  return <li key={toDo.id}>
-                  <span>{toDo.task} <button onClick={()=>deleteTodo(toDo.id)}>Delete</button></span>
-                  </li>
-                })
-            }
-            </ul>
-
-            <button onClick={upperCaseAll}>Update All</button>
-        </div>
+  function doneTask(id){
+    setTodo((prevTodo)=>
+      prevTodo.map((t)=>(
+        t.id === id ? {...t,taskState:!t.taskState}:t
+      ))
     )
+  }
+  
+  return(
+    <>
+      <form action={addTask}>
+        <input type="text" placeholder="Enter the task" name="task"/>
+        &nbsp; &nbsp;
+        <button type="submit">Add Task</button>
+      </form>
+
+      <hr />
+      <h3>List of task :-</h3>
+      <ul>{
+        todos.map((t)=>(
+          <li key={t.id} style={{textDecoration: t.taskState ? "line-through": "none" }}>{t.task} &nbsp; &nbsp; 
+            <Button onClick={()=>doneTask(t.id)} variant="text" sx={{ color: 'green'}}><i className="fa-solid fa-square-check"></i></Button>
+            <Button onClick={()=>deleteTask(t.id)} variant="text" sx={{ color: 'red' }}><i className="fa-solid fa-trash"></i></Button>
+          </li>
+        ))
+      }</ul>
+    </>
+  )
 }
 
 export default ToDoComponent
